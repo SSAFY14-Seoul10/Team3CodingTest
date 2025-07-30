1a522afec49be789270eebb3a65eebc6b33ae037
gaau511 <gaau511@gmail.com>
2025-07-30 15:44:46 +0900

    issue#9 : 양희령 문제 풀이

--- 백준/Huiryeong_9.java
+++ 백준/Huiryeong_9.java
@@ -1,44 +1,43 @@
+
 import java.util.Scanner;
 
 public class Huiryeong_9 {
-    static boolean[][] arr;
-    static int answer;
 
-    static void backTrack(int x, int y) {
-        if (x == arr.length-1) {
-            answer++;
-            return;
-        }
+    static int answer = 0;
+    static int[] arr;
+    static int n;
 
-        for (int i = 0; i < x; i++) {
-            if (arr[i][y]) {
-                return;
-            } else if (y-(x-i) >= 0 && y-(x-i) < arr.length && arr[i][y-(x-i)]) {
-                return;
-            } else if (y+(x-i) >= 0 && y+(x-i) < arr.length && arr[i][y+(x-i)]) {
-                return;
+    public static void backTrack(int x, int y) {
+        if (x == 0) {
+            arr[x] = y;
+            for(int i = 0; i < n; ++i) {
+                backTrack(x+1, i);
+            }
+        } else {
+            for (int i = 0; i < x; ++i) {
+                if (arr[i] == y || arr[i] == y - (x-i) || arr[i] == y + (x - i)) {return;}
+            }
+
+            if (x == n-1) {answer += 1; return;}
+            
+            arr[x] = y;
+            for (int i = 0; i < n; i++) {
+                backTrack(x+1, i);
             }
         }
-
-        arr[x][y] = true;
-        for (int i = 0; i < arr.length; i++) {
-            backTrack(x+1, i);
-        }
-        arr[x][y] = false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        arr = new boolean[n][n];
+        n = sc.nextInt();
+        arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             backTrack(0,i);
         }
 
         System.out.println(answer);
-
         sc.close();
     }
 }
